5e09818f358d38026d639b9a641a34d4f1dbe595
Tin Rabzelj <tin.rabzelj@gmail.com>
2018-08-25 12:12:09 +0200

    Add TOML support

--- cfg/cfg_test.go
+++ cfg/cfg_test.go
@@ -19,7 +19,7 @@ type serviceConfig struct {
 	Factor  float32       `cfg:"factor"`
 }
 
-func TestConfig(t *testing.T) {
+func TestGlob(t *testing.T) {
 	c := New()
 	assert.NoError(t, c.LoadGlob("./*"))
 
@@ -36,9 +36,9 @@ func TestConfig(t *testing.T) {
 	}
 	assert.True(t, reflect.DeepEqual(expected, d))
 }
-func TestJSONConfig(t *testing.T) {
+
+func TestJSON(t *testing.T) {
 	c := New()
-	assert.NoError(t, c.LoadFile("./0_default.yml"))
 	assert.NoError(t, c.LoadJSONString(`{
 		"name": "App",
 		"service": {
@@ -54,7 +54,29 @@ func TestJSONConfig(t *testing.T) {
 		Name: "App",
 		Service: serviceConfig{
 			Port:    4200,
-			Timeout: 5 * time.Second,
+			IDs:     []uint64{1, 2, 42},
+			Factor:  3.14,
+		},
+	}
+	assert.True(t, reflect.DeepEqual(expected, d))
+}
+
+func TestYAML(t *testing.T) {
+	c := New()
+	assert.NoError(t, c.LoadJSONString(`
+name: "App"
+service:
+  port: 4200
+  ids: [1, 2, 42]
+  factor: 3.14
+	`))
+
+	d := config{}
+	assert.NoError(t, c.Decode(&d))
+	expected := config{
+		Name: "App",
+		Service: serviceConfig{
+			Port:    4200,
 			IDs:     []uint64{1, 2, 42},
 			Factor:  3.14,
 		},
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/tinrab/kit
 
 require (
+	github.com/BurntSushi/toml v0.3.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/imdario/mergo v0.3.6
 	github.com/mitchellh/mapstructure v1.0.0
